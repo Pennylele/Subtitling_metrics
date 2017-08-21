5a9480e03949c99a11bb613535f3b821a5722ea1
Penny-Fang <penny.fang@vengaglobal.com>
2017-08-21 17:57:02 +0800

    adding more changes

--- main.js
+++ main.js
@@ -1,10 +1,12 @@
 document.getElementById("onClick").addEventListener("click",                             
 function(){
 var base = document.getElementById("videolength").value
+var target = document.getElementById("targetlanguage").value
 document.getElementById("transcription").value = base*15; /* 15 minutes per min*/
 document.getElementById("SRT").value = base*10; /* 10 minutes per min */
-document.getElementById("ICR").value = base*5; /* 5 minutes per min */
-document.getElementById("LQA").value = base*5; /* 5 minutes per min */
-document.getElementById("total").value = base*35;
-document.getElementById("totalHour").value = base*35/60;
+document.getElementById("ICR").value = base*5*target; /* 5 minutes per min */
+document.getElementById("LQA").value = base*5*target; /* 5 minutes per min */
+document.getElementById("wc").value = base*150; /* 150 words per min */
+document.getElementById("total").value = base*25 + base*10*target
+document.getElementById("totalHour").value = (base*25+base*10*target)/60;
 })
